0d72374a8f59949ad74e8880839ed86cc59b9c0a
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2025-10-28 00:32:55 +0530

    Enable git commit signing

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,7 +4,10 @@
     config = {
       user.name = "Tanmay Vijayvargiya";
       user.email = "12tanmayvijay@gmail.com";
+      user.signkey = "15EE62E8D21043C2";
       init.defaultBranch = "main";
+      commit.gpgsign = true;
+      tag.gpgsign = true;
     };
     lfs.enable = true;
   };
